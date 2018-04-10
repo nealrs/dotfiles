039ae36f0308fe264f3e1e295cd9ee914d273e9a
Neal Shyam <nealrs@users.noreply.github.com>
2018-04-09 20:48:59 -0400

    Update mta.sh

--- mta.sh
+++ mta.sh
@@ -4,22 +4,5 @@
 RESTORE='\033[0m'
 LCYAN='\033[01;36m'
 
-declare -a subway=(
-  "subway/ace"
-  "subway/bdfm"
-  "subway/g"
-  "subway/l"
-  "subway/nqrw"
-  "subway/jz"
-  "subway/123"
-  "subway/456"
-  "subway/7" 
-  "subway/sir" 
-  "subway/s" 
-)
-
 printf "$LCYAN\nNYC SUBWAY STATUS$RESTORE\n"
-for i in "${subway[@]}"
-do
-  curl -s nealrs.herokuapp.com/$i/ | jq -r '.titleText | sub(" SUBWAY "; " ")'
-done
+curl -s nealrs.herokuapp.com/nyc/
